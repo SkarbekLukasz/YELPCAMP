e24a155e928f998543b4cd5fbc8dd48fc70f8e9a
Luke <nycthbre@gmail.com>
2022-12-18 15:07:34 +0100

    Modified campground model with image,desc,price

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany();
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({ 
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, est. Illum voluptatum voluptates, ullam similique facilis neque molestiae culpa suscipit quod, iste voluptate consequatur quos nobis ipsa assumenda porro aliquid!',
+            price
         });
         await camp.save();
     }
